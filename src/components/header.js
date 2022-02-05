d5dd195544b2b476ce868f0568bc76fa0297b3e8
debbieteakle <debbieteakle@gmail.com>
2022-02-05 14:46:28 +1100

    header background stripes

--- src/components/header.js
+++ src/components/header.js
@@ -6,25 +6,25 @@ import Button from "./button.js"
 
 const Header = () => {
 	return (
-			<header className="bg-gray-800	text-white">
-                <StaticImage 
+			<header className="text-white block header-bg-pattern">
+                <div className="flex flex-wrap">
+                {/* <StaticImage 
                 src="../images/background.jpg" 
                 alt="background" 
-                className="flex flex-wrap"
+                className="h-full w-full z0"
                 placeholder="blurred"
                 layout="fullWidth"
-                />
-   
-				<div className="header-profile text-center mt-5 mx-auto mb-0">
-                    <StaticImage 
-                    src="../images/TeakleTutoringLogo-square-600.jpg" 
-                    alt="logo" 
-                    className="profile"
-                    placeholder="blurred"
-                    />
-		
-				</div>
-					<div className="header-title text-center mt-5 mx-auto mb-0">	
+                /> */}
+                    <div className="header-profile text-center mt-5 mx-auto mb-0 z10">
+                        <StaticImage 
+                        src="../images/TeakleTutoringLogo-square-600.jpg" 
+                        alt="logo" 
+                        className="profile bg-center h-full w-full max-w-xs"
+                        placeholder="blurred"
+                        />
+            
+                    </div>
+					<div className="header-title text-center mt-5 mx-auto mb-0 z-10">	
 					    <h1 className="text-4xl">Mathew Teakle</h1>
 					    <h2 className="text-2xl">Sydney HSC English Tutor</h2>
 						<p className="text-lg p-6">Available Sunday - Friday  |  Online or In Person</p>
@@ -32,6 +32,7 @@ const Header = () => {
 					        <Button name="Enquire Now"/>
 					    </Link>
 					</div>
+                </div>
 		    </header>
 
 	);
