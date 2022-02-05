13e2d0fc1a4faa173d7b1c4884228002f67237a4
debbieteakle <debbieteakle@gmail.com>
2022-02-05 15:49:47 +1100

    adding helmet to home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import * as React from "react"
+import { Helmet } from "react-helmet"
 import { graphql } from 'gatsby'
 import { Link } from "gatsby"
 import Layout from "../components/layout"
@@ -16,7 +17,17 @@ import { FaQuoteLeft } from 'react-icons/fa';
 const IndexPage = ({data}) => {
   return (
     <Layout>
-      <title>Teakle Tutoring</title>
+      <Helmet>
+		          <meta charSet="utf-8" />
+		          <title>Mathew Teakle Tutoring | Online HSC English Tuition, Sydney</title>
+		          <link rel="canonical" href="https://teakle.com.au" />
+		          <meta name="keywords" content="Find a Tutor, English Tutor, English Teacher, 
+		          Private Tutor English, Online Teacher English, Private English Teacher, HSC English Tutor, 
+		          High School English, High School Literacy Tutor, Essays, Creative Writing" />
+		          <meta name="description" content="Mathew Teakle is a qualified English teacher with 20 years of tutoring experience. 
+		          He provides private English tuition online to students all over NSW. Pay week by week or by term. Contact Mathew to discuss your English tutoring needs." />
+		          <meta name="google-site-verification" content="pIfTVRhYnx_exObArZVnORzf_3KokccntYBpdYTqUzo" />
+		  </Helmet>
       <main >
         <Header />
         <Enrolments />
